52426ea8786fd969a58fd35f02542abfc4a5d8a9
Flutter0599 <93052560+Flutter0599@users.noreply.github.com>
2021-10-27 14:13:18 +0300

    c5

--- lib/model/address_model.dart
+++ lib/model/address_model.dart
@@ -3,13 +3,11 @@ class Address {
   final String? lastName;
   final String? phone;
   final String? city;
-  final String? zipy;
 
   Address({
     this.fisrtName,
     this.lastName,
     this.phone,
     this.city,
-    this.zipy,
   });
 }
